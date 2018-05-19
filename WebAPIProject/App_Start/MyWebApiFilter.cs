4c5ffb9efd1daf751db5430710d235448895965f
Ranjeetab-17 <ranjeetab17@gmail.com>
2018-05-19 23:54:20 +0530

    onActionExecuted changes

--- WebAPIProject/App_Start/MyWebApiFilter.cs
+++ WebAPIProject/App_Start/MyWebApiFilter.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
+using WebAPIProject.Models;
 
 namespace WebAPIProject.App_Start
 {
@@ -16,22 +19,14 @@ namespace WebAPIProject.App_Start
     {
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
+            var header = actionContext.Request.Headers.SingleOrDefault(x => x.Key == "CheckSum").Value.SingleOrDefault();
+            var datetTimeStamp = actionContext.Request.Headers.SingleOrDefault(x => x.Key == "DateTimeStamp").Value.SingleOrDefault();             
             var actionName = actionContext.ActionDescriptor.ActionName;
-            var userId = actionContext.ActionArguments.Values;
-            var Password = actionContext.ActionArguments.Values;
-            //var objectContent = actionContext.
-        }
-
-        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
-        {
-            var objectContent = actionExecutedContext.Response.Content as ObjectContent;
-            if (objectContent != null)
-            {
-                var type = objectContent.ObjectType; //type of the returned object
-                var value = objectContent.Value; //holding the returned value
-            }
-
-            Debug.WriteLine("ACTION 1 DEBUG  OnActionExecuted Response " + actionExecutedContext.Response.StatusCode.ToString());
-        }
+            var controllerName = actionContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            var userInput = (DefaultInput)actionContext.ActionArguments.Values.AsQueryable().SingleOrDefault();
+            if (userInput.UserID == "Test" && userInput.Password == "Powai")
+                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.Accepted, "CheckSum Success");
+            actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.PreconditionFailed, "CheckSum Failed");
+        }       
     }
 }
